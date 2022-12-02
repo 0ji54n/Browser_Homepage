dbcb170280a655cc2ab13b0df702fe00556ca03b
0ji54n <92131644+0ji54n@users.noreply.github.com>
2022-12-02 08:20:50 +0000

    changing background

--- src/background.js
+++ src/background.js
@@ -1,9 +1,11 @@
 import {extendTheme} from "@chakra-ui/react";
-const theme = extendTheme ({
-    global: props => ({
-        body:{
-            bg:"#3b1418"(props),
+const Theme = extendTheme ({
+    styles:{
+        global:{
+            body:{
+                bg:"#3b1418"
+            }
         }
-    })
+    },
 })
-export default theme
+export default Theme
